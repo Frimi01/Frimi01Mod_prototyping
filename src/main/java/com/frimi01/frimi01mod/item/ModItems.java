b580970d55b00c851ef6a9ac86fcb078a9406a09
Frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-04-03 23:44:03 +0200

    Update items

--- src/main/java/com/frimi01/frimi01mod/item/ModItems.java
+++ src/main/java/com/frimi01/frimi01mod/item/ModItems.java
@@ -1,17 +1,41 @@
 package com.frimi01.frimi01mod.item;
 
 import com.frimi01.frimi01mod.Frimi01Mod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-public class ModItems {
+import java.util.function.Function;
 
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(Frimi01Mod.MOD_ID, name), item);
+public class ModItems {
+    public static final Item DARK_GEM = register("dark_gem", Item::new, new Item.Settings());
+    public static final Item DARK_MINERAL = register("dark_mineral", Item::new, new Item.Settings());
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Frimi01Mod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
     }
+
+
     public static void registerModItems(){
         Frimi01Mod.LOGGER.info("Registering Mod Items for " + Frimi01Mod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(DARK_GEM);
+            fabricItemGroupEntries.add(DARK_MINERAL);
+        });
     }
 }
